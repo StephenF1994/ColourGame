6be33cb5b4cc9ee306eae18247e5fc5a891939f5
Stephen Finegan <stephenfinegan@Stephens-MacBook-Air.local>
2016-05-18 22:01:36 +0100

    New right answer generated on refresh, on user win, all squares change to correct colour.

--- colourGame.js
+++ colourGame.js
@@ -9,7 +9,7 @@ var colours = [
 
 
 var squares = document.querySelectorAll(".square");				//Selects all the elements with class square in the document, puts them in var squares
-var picked = colours[3];
+var picked = pickColor();
 var ColourDisplay = document.getElementById('ColourDisplay');
 ColourDisplay.textContent = picked;
 
@@ -22,16 +22,29 @@ for(var i = 0; i<squares.length; i++){							//Cycles through array of squares a
 		//get colour of clicked square
 		var clickedColour = this.style.background;
 		//compare the clicked colour to previously set answer.
-		if(clickedColour === picked){
-			alert("correct")
+		if(clickedColour === picked){							//if the right answer is picked
+			message.textContent="Correct";						//message set to correct
+			changeColours();									//change all the colours*/
 		}
 		else{
-			alert("wrong");
+			this.style.background = "#232323";
+			message.textContent = "Try again";
 		}
 
 			})
 		
 
 	
+}
+
+function changeColours(){
+	for(var i = 0; i<squares.length; i++){
+		squares[i].style.background = picked;				//When right answer selected, change all to that colour
+	}
+}
+
+function pickColor(){
+	var random =  Math.floor(Math.random()*colours.length);
+	return colours[random];
 }
 
